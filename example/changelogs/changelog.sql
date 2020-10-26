d96e3e535ed8b79d501c5d9365e278cafdd8fdae
Adeel Malik <1605783+adeelmalik78@users.noreply.github.com>
2020-10-26 16:32:20 -0500

    Update changelog.sql

--- example/changelogs/changelog.sql
+++ example/changelogs/changelog.sql
@@ -28,6 +28,3 @@ alter table person add column country varchar(2)
 --rollback ALTER TABLE person DROP COLUMN state;
 alter table person add column state varchar(2)
 
---changeset other.dev:5
---rollback ALTER TABLE company DROP COLUMN country;
-alter table company add column country varchar(2)
