ca3bee8ff0d434e3048bbe78e0e1d84495d9d099
kz <kateryna_z@yahoo.com>
2021-07-21 21:48:11 -0400

    kz: Adding new changeset for example

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset your.name:1
+--changeset kz:1
 --rollback DROP TABLE person;
 create table person (
     id int primary key,
@@ -10,7 +10,7 @@ create table person (
     city varchar(30)
 )
 
---changeset your.name:2
+--changeset kz:2
 --rollback DROP TABLE company;
 create table company (
     id int primary key,
@@ -20,14 +20,14 @@ create table company (
     city varchar(30)
 )
 
---changeset other.dev:3
+--changeset kz:3
 --rollback ALTER TABLE person DROP COLUMN country;
 alter table person add column country varchar(2)
 
---changeset other.dev:4
+--changeset kz:4
 --rollback ALTER TABLE person DROP COLUMN state;
 alter table person add column state varchar(2)
 
---changeset other.dev:5
+--changeset kz:5
 --rollback ALTER TABLE company DROP COLUMN country;
 alter table company add column country varchar(2)
