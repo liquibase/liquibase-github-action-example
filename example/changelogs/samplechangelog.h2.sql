36f3e6fd3cb736c8bef4ea7ba48e31e060fdf4d4
Derek Smart <derek@grindaga.com>
2023-08-02 14:37:45 -0400

    add runWith:psql to samplechangelog.h2.sql

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset your.name:1
+--changeset your.name:1 runWith:psql
 --rollback DROP TABLE person;
 create table person (
     id int primary key,
@@ -10,7 +10,7 @@ create table person (
     city varchar(30)
 )
 
---changeset your.name:2
+--changeset your.name:2 runWith:psql
 --rollback DROP TABLE company;
 create table company (
     id int primary key,
@@ -20,14 +20,14 @@ create table company (
     city varchar(30)
 )
 
---changeset other.dev:3
+--changeset other.dev:3 runWith:psql
 --rollback ALTER TABLE person DROP COLUMN country;
 alter table person add column country varchar(2)
 
---changeset other.dev:4
+--changeset other.dev:4 runWith:psql
 --rollback ALTER TABLE person DROP COLUMN state;
 alter table person add column state varchar(2)
 
---changeset other.dev:5
+--changeset other.dev:5 runWith:psql
 --rollback ALTER TABLE company DROP COLUMN country;
 alter table company add column country varchar(2)
