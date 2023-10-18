ce719a0e755f513665489ee52e19626d3c494efd
Michael Beatty <michael.beatty@sherwin.com>
2023-10-18 11:37:23 -0400

    mxb: Adding new changeset for example

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -31,3 +31,10 @@ alter table person add column state varchar(2)
 --changeset other.dev:5
 --rollback ALTER TABLE company DROP COLUMN country;
 alter table company add column country varchar(2)
+
+--changeset mxb.mxb1
+--rollback DROP TABLE yourname;
+CREATE TABLE yourname (
+	id int primary key,
+	name varchar(50) not null
+)
